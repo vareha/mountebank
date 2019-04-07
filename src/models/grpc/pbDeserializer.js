51a1233a5a4a377236fc852afa1a191c534bf49b
George Bills <george.s.bills@gmail.com>
2019-04-07 21:38:47 +1000

    Stub out pbDeserializer.

---
+++ src/models/grpc/pbDeserializer.js
@@ -0,0 +1,41 @@
+'use strict';
+
+function create(messageDefinition) {
+    const jspb = require('google-protobuf'),
+        fields = Object.keys(messageDefinition).reduce((arr, key, idx) => {
+            const definition = messageDefinition[key],
+                i = definition.i || idx;
+            arr[i] = { name: key, ...definition };
+            return arr;
+        }, [])
+    return buffer => {
+        const msg = {},
+            bytes = new Uint8Array(buffer),
+            reader = new jspb.BinaryReader(bytes);
+        while (reader.nextField()) {
+            if (reader.isEndGroup()) {
+                break;
+            }
+            const field = reader.getFieldNumber(),
+                definition = fields[field];
+            if (!definition) {
+                reader.skipField();
+                continue;
+            }
+            const type = definition.type || "STRING",
+                name = definition.name;
+            switch (type) {
+                case "STRING":
+                    msg[name] = reader.readString();
+                    break;
+                default:
+                    throw new Error("unrecognized protobuf field type: " + type)
+            }
+        }
+        return msg;
+    };
+}
+
+module.exports = {
+    create: create,
+}
