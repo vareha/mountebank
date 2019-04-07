62ff7d7fd3f43c38eadd7ec9c7e80c6c07bae86b
George Bills <george.s.bills@gmail.com>
2019-04-07 21:53:20 +1000

    serialize a basic string test

---
+++ test/models/grpc/pbSerializerTest.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const assert = require('assert'),
+    pbSerializer = require('../../../src/models/grpc/pbSerializer'),
+    promiseIt = require('../../testHelpers').promiseIt;
+
+describe('pbSerializer', function () {
+    describe('#create', function () {
+        it('should correctly serialize a basic string message', function () {
+            const definition = { name: { i: 1, type: "STRING" } },
+                message = { name: "john smith" },
+                serializer = pbSerializer.create(definition),
+                out = serializer(message),
+                expected = new Uint8Array([10, 10, 106, 111, 104, 110, 32, 115, 109, 105, 116, 104]);
+            assert.equal(out.toString(), expected.toString());
+        });
+    });
+});
