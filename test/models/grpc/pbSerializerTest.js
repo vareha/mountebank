ebb8f19cbce8f2a1c6e115c17af8f090b7f0d06b
George Bills <george.s.bills@gmail.com>
2019-04-07 21:54:17 +1000

    should assume the STRING type test

--- test/models/grpc/pbSerializerTest.js
+++ test/models/grpc/pbSerializerTest.js
@@ -6,8 +6,18 @@ const assert = require('assert'),
 
 describe('pbSerializer', function () {
     describe('#create', function () {
+
         it('should correctly serialize a basic string message', function () {
             const definition = { name: { i: 1, type: "STRING" } },
+                message = { name: "john smith" },
+                serializer = pbSerializer.create(definition),
+                out = serializer(message),
+                expected = new Uint8Array([10, 10, 106, 111, 104, 110, 32, 115, 109, 105, 116, 104]);
+            assert.equal(out.toString(), expected.toString());
+        });
+
+        it('should assume the STRING type', function () {
+            const definition = { name: { i: 1 } },
                 message = { name: "john smith" },
                 serializer = pbSerializer.create(definition),
                 out = serializer(message),
