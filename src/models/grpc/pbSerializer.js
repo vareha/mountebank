d0a8bc8760ce7de2b6f5a33751d8f79ad2d09fd7
George Bills <george.s.bills@gmail.com>
2019-04-07 21:35:45 +1000

    Stub out pb serializer.

---
+++ src/models/grpc/pbSerializer.js
@@ -0,0 +1,35 @@
+'use strict';
+
+function create(messageDefinition) {
+    const jspb = require('google-protobuf');
+    return msg => {
+        const writer = new jspb.BinaryWriter()
+        Object.keys(msg).forEach((key) => {
+            const definition = messageDefinition[key];
+            if (!definition) {
+                throw new Error("no definition for field: " + key)
+            }
+            const idx = definition.i,
+                type = definition.t || "STRING",
+                val = msg[key];
+            switch (type) {
+                case "STRING":
+                    writer.writeString(idx, val);
+                    break;
+                case "UINT32":
+                    writer.writeUint32(idx, val);
+                    break;
+                case "BOOL":
+                    writer.writeBool(idx, val);
+                    break;
+                default:
+                    throw new Error("unrecognized protobuf field type: " + type)
+            }
+        });
+        return writer.getResultBuffer();
+    };
+}
+
+module.exports = {
+    create: create,
+};
