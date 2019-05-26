01f62d0448728a2a413fac2150650484cbb2e8ee
George Bills <george.s.bills@gmail.com>
2019-05-26 17:39:29 +1000

    should parse helloworld imposter

--- functionalTest/api/grpc/grpcImposterTest.js
+++ functionalTest/api/grpc/grpcImposterTest.js
@@ -7,6 +7,23 @@ const assert = require('assert'),
     port = api.port + 1,
     timeout = parseInt(process.env.MB_SLOW_TEST_TIMEOUT || 2000);
 
+const helloworldImposter = {
+    host: '127.0.0.1',
+    port: 4545,
+    protocol: 'grpc',
+    protos: {
+        helloworld: {
+            Greeter: {
+                methods: {
+                    SayHello: { requestType: 'HelloRequest', responseType: 'HelloReply' }
+                }
+            },
+            HelloRequest: { fields: { 'name': { type: 'string', id: 1 } } },
+            HelloReply: { fields: { 'message': { type: 'string', id: 1 } } }
+        }
+    }
+}
+
 describe('grpc imposter', () => {
 
     describe('POST /imposters/:id', () => {
@@ -28,6 +45,13 @@ describe('grpc imposter', () => {
             return api.post('/imposters', request).then(response => {
                 assert.strictEqual(response.statusCode, 201);
                 assert.ok(response.body.port == 1234);
+            }).finally(() => api.del('/imposters'));
+        });
+
+        promiseIt('should parse helloworld imposter', () => {
+            return api.post('/imposters', helloworldImposter).then(response => {
+                assert.strictEqual(response.statusCode, 201);
+                assert.ok(response.body.port == 4545);
             }).finally(() => api.del('/imposters'));
         });
     });
